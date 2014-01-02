8cb5fa4ca2ce13547ae965934eab911416d34dff
arafatm <github@arafatm.com>
2014-01-02 00:40:18 +0000

    include secret_token for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vagrant::Application.config.secret_key_base = '046a02f58be7d01f9a21ca03d2a5d5e00ff7bdfb7e7f1d4c2387eaabc025e8afeb1449f17de2964c0a8c7f77f70991d0008ee43bcd77bd95d191c45859b16876'
